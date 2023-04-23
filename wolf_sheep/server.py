5e23835420621ba0d3584b82fbff1427b8361d86
salmamob <116468037+salmamob@users.noreply.github.com>
2023-04-23 13:33:54 -0400

    changed "text" to "energy" since that is what it shows

--- wolf_sheep/server.py
+++ wolf_sheep/server.py
@@ -14,27 +14,27 @@ def wolf_sheep_portrayal(agent):
         portrayal["Shape"] = "wolf_sheep/resources/sheep.png"
         # https://icons8.com/web-app/433/sheep
         portrayal["Animal"] = "Sheep"
-        portrayal["scale"] = 0.9
+        portrayal["Scale"] = 0.9
         portrayal["Layer"] = 2
-        portrayal["text"] = round(agent.energy, 1)
+        portrayal["Energy"] = round(agent.energy, 1)
         portrayal["text_color"] = "White"
 
     elif type(agent) is Wolf:
         portrayal["Shape"] = "wolf_sheep/resources/wolf.png"
         # https://icons8.com/web-app/36821/German-Shepherd
         portrayal["Animal"] = "Wolf"
-        portrayal["scale"] = 0.9
+        portrayal["Scale"] = 0.9
         portrayal["Layer"] = 2
-        portrayal["text"] = round(agent.energy, 1)
+        portrayal["Energy"] = round(agent.energy, 1)
         portrayal["text_color"] = "White"
 
     elif type(agent) is Cheetah:
         portrayal["Shape"] = "wolf_sheep/resources/cheetah.png"
         # https://icons8.com/web-app/36821/German-Shepherd
         portrayal["Animal"] = "Cheetah"
-        portrayal["scale"] = 0.9
+        portrayal["Scale"] = 0.9
         portrayal["Layer"] = 2
-        portrayal["text"] = round(agent.energy, 1)
+        portrayal["Energy"] = round(agent.energy, 1)
         portrayal["text_color"] = "White"
 
     elif type(agent) is GrassPatch:
